aaf931904cc1bec8952d9d15b499bc6d9f827dea
lilu <chujunhe1234@163.com>
2022-03-31 19:02:25 +0800

    add login view

--- LLClient/local_pods/LLMain/LLMain.podspec
+++ LLClient/local_pods/LLMain/LLMain.podspec
@@ -44,4 +44,5 @@ TODO: Add long description of the pod here.
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
   s.dependency 'LLCommon'
+  s.dependency 'SnapKit'
 end
--- LLClient/local_pods/LLMain/LLMain/Classes/Login/Controller/LoginViewController.swift
+++ LLClient/local_pods/LLMain/LLMain/Classes/Login/Controller/LoginViewController.swift
@@ -7,10 +7,44 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 public class LoginViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
-        print("LoginViewController")
+        setUpViews()
     }
+    
+    func setUpViews() {
+        view.addSubview(imageView)
+        view.addSubview(label)
+        
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(80)
+            make.top.equalToSuperview().offset(40)
+            make.centerX.equalToSuperview()
+        }
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(imageView.snp.bottom).offset(20)
+        }
+    }
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView.init()
+        imageView.image = .init(named: "icon-256")
+        return imageView
+    }()
+    
+    lazy var label: UILabel = {
+        let l = UILabel.init()
+        l.text = "请先登录"
+        l.textColor = .orange
+        l.font = .boldSystemFont(ofSize: 20)
+        l.sizeToFit()
+        return l
+    }()
+    
+    
 }
