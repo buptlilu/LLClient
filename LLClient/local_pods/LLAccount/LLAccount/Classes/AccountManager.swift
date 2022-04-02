ffe7977372d8521de93e7b92d35d9483cdbd6e81
lilu <chujunhe1234@163.com>
2022-04-02 10:17:55 +0800

    account init

--- LLClient/local_pods/LLAccount/LLAccount/Classes/AccountManager.swift
+++ LLClient/local_pods/LLAccount/LLAccount/Classes/AccountManager.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import LLCommon
 
 public class AccountManager: NSObject {
     public static var shared = AccountManager.init()
@@ -15,12 +15,10 @@ public class AccountManager: NSObject {
     
     public override init() {
         super.init()
-        accounts = allAccounts()
+        loadAccounts()
     }
     
     public func allAccounts() -> [Account]{
-        var accounts = [Account].init()
-        //todo
         return accounts
     }
     
@@ -33,7 +31,22 @@ public class AccountManager: NSObject {
     }
     
     public func saveAllAccounts() {
-        //todo
+        DispatchQueue.global().async {
+            if let path = self.accountFilePath()  {
+                var models = [Account].init()
+                for account in self.accounts {
+                    if account.isTokenValid() {
+                        models.append(account)
+                    }
+                }
+                do {
+                    let datasToWrite = try JSONEncoder().encode(models)
+                    try datasToWrite.write(to: path)
+                } catch {
+                    Logger.error("fail to save accounts\(error)")
+                }
+            }
+        }
     }
     
     public func saveAccount(_ account: Account) {
@@ -43,5 +56,25 @@ public class AccountManager: NSObject {
         
         accounts.append(account)
         saveAllAccounts()
+    }
+    
+    public func loadAccounts() {
+        if let path = self.accountFilePath()  {
+            do {
+                let datas = try Data.init(contentsOf: path)
+                let models = try JSONDecoder().decode([Account].self, from: datas)
+                self.accounts = models
+                self.saveAllAccounts()
+            } catch {
+                Logger.error("fail to load accounts\(error)")
+            }
+        }
+    }
+    
+    func accountFilePath() -> URL? {
+        var path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        path?.appendPathComponent("accounts")
+        Logger.info("account path:\(path?.absoluteString)")
+        return path
     }
 }
